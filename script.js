b220ee0af19cd06101c7a61540fabf96eaf12568
Andrew Tran <andrewtranmsw@gmail.com>
2022-03-06 14:19:58 -0800

    leaflet

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibmRyd3RybiIsImEiOiJjbDA3bnZkODUyNGI1M2RyczBtaHI2bGV4In0.CGjP2zwrhf3Cq5SVSeqyzw';
+const mapboxToken =
+  "pk.eyJ1IjoibmRyd3RybiIsImEiOiJjbDA3bnZkODUyNGI1M2RyczBtaHI2bGV4In0.CGjP2zwrhf3Cq5SVSeqyzw";
 
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-    enableHighAccuracy: true
-})
+//Leaflet API below:
+const map = L.map("map").setView([47.606, -122.332], 12.5);
 
-function successLocation(position) {
-    console.log(position);
-    setupMap([position.coords.longitude, position.coords.latitude])
-}
+//Mapbox Streets tile layer
+L.tileLayer(
+  `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${mapboxToken}`,
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: mapboxToken,
+  }
+).addTo(map);
 
-function errorLocation() {
-    setupMap([47.60, 122.33])
-}
+//Creates a 'pin' on where the coordinates are:
+let markerTest = L.marker([47.606, -122.332]).addTo(map);
+let markerTest2 = L.marker([47.609, -122.331]).addTo(map);
+let markerTest3 = L.marker([47.603, -122.328]).addTo(map);
+let markerTest4 = L.marker([47.609, -122.332]).addTo(map);
 
-function setupMap(center) {
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: center,
-        zoom: 15
-      })
+//Creates a 'circle' on where the coordinates are:
 
-      const nav = new mapboxgl.NavigationControl()
-      map.addControl(nav)
-}
+const circle = L.circle([47.606, -122.332], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 500,
+}).addTo(map);
+
+//Pop ups on the marker and the circle
+markerTest
+  .bindPopup("<b>Playdate happening with my pitbull woof!</b>")
+  .openPopup();
+markerTest2.bindPopup("<b>I'm number 2</b>").openPopup();
+markerTest3.bindPopup("<b>Playdate happening with my Golden!</b>").openPopup();
+markerTest4.bindPopup("<b>I'm number 4</b>").openPopup();
+circle.bindPopup("Seattle Park");
+
+//popup layer when you need something more than attaching a popup to an object
+// const popup = L.popup()
+//   .setLatLng([47.606, -122.332])
+//   .setContent("I am a standalone popup.")
+//   .openOn(map);
